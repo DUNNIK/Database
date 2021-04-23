f5e07bddcd8090e317d3c9407cbdfcb208262f78
DUNNIK <dns.56@mail.ru>
2021-04-23 19:07:39 +0300

    edit: mockito

--- src/main/java/com/itmo/java/basics/Program.java
+++ src/main/java/com/itmo/java/basics/Program.java
@@ -5,8 +5,6 @@ import com.itmo.java.basics.console.impl.ExecutionEnvironmentImpl;
 import com.itmo.java.basics.exceptions.DatabaseException;
 import com.itmo.java.basics.initialization.Initializer;
 import com.itmo.java.basics.initialization.impl.*;
-import com.itmo.java.basics.logic.Database;
-import com.itmo.java.basics.logic.impl.DatabaseImpl;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -30,7 +28,6 @@ public class Program {
                     .build());
 
 
-            Database database = DatabaseImpl.create("database2", executionEnvironmentPath);
             initializer.perform(InitializationContextImpl.builder()
                     .executionEnvironment(executionEnvironment)
                     .build());
