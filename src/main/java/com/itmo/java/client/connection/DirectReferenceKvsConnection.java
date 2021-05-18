704777e5ce3388831ab5fba82f91e0323a78620e
DUNNIK <dns.56@mail.ru>
2021-05-18 12:33:19 +0300

    add: realisation DirectReferenceKvsConnection

--- src/main/java/com/itmo/java/client/connection/DirectReferenceKvsConnection.java
+++ src/main/java/com/itmo/java/client/connection/DirectReferenceKvsConnection.java
@@ -1,9 +1,13 @@
 package com.itmo.java.client.connection;
 
 import com.itmo.java.basics.DatabaseServer;
+import com.itmo.java.basics.console.DatabaseCommand;
+import com.itmo.java.basics.console.DatabaseCommandResult;
 import com.itmo.java.client.exception.ConnectionException;
 import com.itmo.java.protocol.model.RespArray;
 import com.itmo.java.protocol.model.RespObject;
+
+import java.util.concurrent.ExecutionException;
 
 /**
  * Реализация подключения, когда есть прямая ссылка на объект
@@ -18,8 +22,14 @@ public class DirectReferenceKvsConnection implements KvsConnection {
 
     @Override
     public RespObject send(int commandId, RespArray command) throws ConnectionException {
-
-        return null;
+        DatabaseCommandResult result;
+        try {
+            result = databaseServer.executeNextCommand(command).get();
+        } catch (InterruptedException | ExecutionException e) {
+            Thread.currentThread().interrupt();
+            throw new ConnectionException("An error occurred while connecting to the database server", e);
+        }
+        return result.serialize();
     }
 
     /**
@@ -27,5 +37,6 @@ public class DirectReferenceKvsConnection implements KvsConnection {
      */
     @Override
     public void close() {
+        //Do nothing because we haven't got such connection
     }
 }
