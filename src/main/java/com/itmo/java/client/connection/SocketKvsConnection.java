204e732a327872500972503dabb16da5219c3794
DUNNIK <dns.56@mail.ru>
2021-06-10 23:36:40 +0300

    add: throw Exception in SocketKvsConnection

--- src/main/java/com/itmo/java/client/connection/SocketKvsConnection.java
+++ src/main/java/com/itmo/java/client/connection/SocketKvsConnection.java
@@ -18,6 +18,9 @@ public class SocketKvsConnection implements KvsConnection {
 
     public SocketKvsConnection(ConnectionConfig config) {
         try {
+            if (config.getHost() == null) {
+                throw new IOException("Host is empty");
+            }
             clientSocket = new Socket(config.getHost(), config.getPort());
         } catch (IOException e) {
             e.printStackTrace();
