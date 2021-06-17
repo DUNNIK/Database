87f75e04edf1a8b67e7be4862ee524fe07892c80
DUNNIK <dns.56@mail.ru>
2021-06-17 10:50:02 +0300

    add: checking for thread interrupts

--- src/main/java/com/itmo/java/basics/connector/JavaSocketServerConnector.java
+++ src/main/java/com/itmo/java/basics/connector/JavaSocketServerConnector.java
@@ -66,7 +66,7 @@ public class JavaSocketServerConnector implements Closeable {
     }
 
     private boolean threadIsNotInterrupted() {
-        return !Thread.currentThread().isInterrupted();
+        return !clientIOWorkers.isShutdown() || !connectionAcceptorExecutor.isShutdown();
     }
 
     /**
